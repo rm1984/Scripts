dbcf45373d11be537d88b4c478ad60fba9631b8e
Riccardo Mollo <riccardomollo84@gmail.com>
2018-12-30 11:40:43 +0100

    First commit

---
+++ Hacking/Info_Gathering/zmap-screenshot.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# zmap-screenshot - A script that takes screenshots
+#
+# Author:   Riccardo Mollo
+# ------------------------------------------------------------------------------
+
+TARGET=$1
+PORT=$2
+THIS=$(basename "$0")
+
+if [[ -z "$TARGET" ]] ; then
+#    echo "Usage:    $THIS <target> [<port>] [<out_dir>]"
+    echo "Usage:    $THIS <target> [<port>]"
+    echo
+    echo "          <target>     -   Initial host (eg: scanme.nmap.org)"
+    echo "          <port>       -   Port to check (default: 80)"
+#    echo "          <out_dir>    -   Directory where screnshot are saved (default: /tmp/shots)"
+
+    exit 1
+fi
+
+echo "Initial target:         $TARGET"
+
+if [[ -z "$PORT" ]] ; then
+    PORT=80
+
+    echo "Port:                   $PORT (default)"
+else
+    echo "Port:                   $PORT"
+fi
+
+IP=$(host $TARGET | grep 'has address' | grep -v 'IPv6' | grep -v 'NXDOMAIN' | awk '{print $NF}' | head -1)
+
+if [[ -z "$IP" ]] ; then
+    echo "Wrong target format or target not resolvable."
+
+    exit 1
+fi
+
+OUT_DIR="/tmp/shots_${TARGET}"
+mkdir $OUT_DIR
+
+echo "Target's IP address:    $IP"
+
+RANGE=$(whois $IP | egrep 'inetnum:|NetRange:' | head -1 | cut -d':' -f2 | xargs | tr -d ' ')
+
+echo "IP addresses range:     $RANGE"
+
+NETWORK=$(ipcalc "$RANGE" | tail -1)
+
+echo "Range network:          $NETWORK"
+
+FILENAME="${TARGET}_$(echo "$NETWORK" | sed -e 's/\//_/g').txt"
+
+sudo zmap -p $PORT -o $FILENAME -q --disable-syslog "${NETWORK}"
+
+echo "Discovered IPs file:    $FILENAME"
+echo "Discovered hosts   :    $(cat $FILENAME | wc -l)"
+
+for IP in $(cat $FILENAME) ; do
+    TEST_HTTPS=$(amap -1 -q $IP $PORT | grep 'matches ssl')
+    TEST_HTTPS=$?
+    TEST_HTTP=$(amap -1 -q $IP $PORT | grep 'matches http')
+    TEST_HTTP=$?
+
+    if [[ $TEST_HTTPS -eq 0 && $TEST_HTTP -ne 0 ]] ; then           # HTTPS
+        PROTOCOL="https"
+    elif [[ $TEST_HTTP -eq 0 && $TEST_HTTPS -ne 0 ]] ; then         # HTTP
+        PROTOCOL="http"
+    elif [[ $TEST_HTTP -eq 0 && $TEST_HTTPS -eq 0 ]] ; then         # probably HTTPS
+        PROTOCOL="https"
+    else
+        echo "Skipping host $IP (unknown protocol for port $PORT)"
+
+        continue
+    fi
+
+    echo "Saving screenshot for site: $PROTOCOL://$IP:$PORT"
+
+    cutycapt --insecure --out=$OUT_DIR/$IP.jpg --smooth --private-browsing=on --max-wait=5000 --url="$PROTOCOL://$IP:$PORT" 2> /dev/null
+done
+
