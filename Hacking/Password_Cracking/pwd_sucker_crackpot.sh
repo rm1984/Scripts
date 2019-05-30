bbf09e4191bd3776df8a9334d8a251e2f962a27c
Riccardo Mollo <riccardo.mollo@securegroup.it>
2019-05-30 08:46:49 +0200

    ...

--- Hacking/Password_Cracking/pwd_sucker_crackpot.sh
+++ Hacking/Password_Cracking/pwd_sucker_crackpot.sh
@@ -1,13 +1,52 @@
 #!/usr/bin/env bash
+#
+# Author:       Riccardo Mollo (riccardomollo84@gmail.com)
+#
+# Name:	        pwd_sucker_crackpot.sh
+#
+# Description:  A script that fetches cracked passwords from the following site:
+#               http://cracker.offensive-security.com/index.php
+#
+#
+# --TODO--
+# - ???
+#
+#
+################################################################################
 
-[[ $DEBUG -ne 0 ]] && set -x
+
+# VARIABLES --------------------------------------------------------------------
 
 URL="http://cracker.offensive-security.com/index.php"
 DICT_DIR=~/dictionaries
 DICT=$DICT_DIR/CUSTOM_crackpot.txt
+
+
+# FUNCTIONS --------------------------------------------------------------------
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1 || { echo "Command not found: $1" 1>&2 ; exit 1 ; }
+}
+
+
+# CHECKS -----------------------------------------------------------------------
+
+declare -a CMDS=(
+"wget"
+);
+
+for CMD in ${CMDS[@]} ; do
+    command_exists $CMD
+done
+
+
+# MAIN -------------------------------------------------------------------------
+
+[[ $DEBUG -ne 0 ]] && set -x
+
 OUT_HTML=$(echo /tmp/.rnd-${RANDOM}.html)
 TMP=/tmp/.crackpot.txt
-SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 wget -O $OUT_HTML $URL > /dev/null 2>&1
 $SDIR/htmltable2csv.py $OUT_HTML > /dev/null 2>&1
--- Hacking/Password_Cracking/pwd_sucker_md5db.sh
+++ Hacking/Password_Cracking/pwd_sucker_md5db.sh
@@ -1,10 +1,48 @@
 #!/usr/bin/env bash
+#
+# Author:       Riccardo Mollo (riccardomollo84@gmail.com)
+#
+# Name:	        pwd_sucker_md5db.sh
+#
+# Description:  A script that fetches cracked passwords from the following site:
+#               https://www.nitrxgen.net/md5db/
+#
+#
+# --TODO--
+#
+#
+################################################################################
 
-[[ $DEBUG -ne 0 ]] && set -x
+
+# VARIABLES --------------------------------------------------------------------
 
 URL="https://www.nitrxgen.net/md5db/"
 DICT_DIR=~/dictionaries
 DICT=$DICT_DIR/CUSTOM_md5db.txt
+
+
+# FUNCTIONS --------------------------------------------------------------------
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1 || { echo "Command not found: $1" 1>&2 ; exit 1 ; }
+}
+
+
+# CHECKS -----------------------------------------------------------------------
+
+declare -a CMDS=(
+"wget"
+);
+
+for CMD in ${CMDS[@]} ; do
+    command_exists $CMD
+done
+
+
+# MAIN -------------------------------------------------------------------------
+
+[[ $DEBUG -ne 0 ]] && set -x
+
 OUT_HTML=$(echo /tmp/.rnd-${RANDOM}.html)
 
 wget $URL -O $OUT_HTML
