732b6992215c338c8099251ceef81163bb529091
Riccardo Mollo <riccardo.mollo@securegroup.it>
2020-09-16 10:35:55 +0200

    Massive code changes and some fixes

--- Hacking/Password_Cracking/autojohn.sh
+++ Hacking/Password_Cracking/autojohn.sh
@@ -9,18 +9,18 @@
 #               At the moment, it is solely intended for dictionary attacks.
 #
 # Usage:        ./autojohn.sh --help|-h
-#               ./autojohn.sh --info
 #               ./autojohn.sh <HASHES_FILE>
 #               ./autojohn.sh <HASHES_FILE> <FORMAT> <SESSION_NAME> [<RULE>]
+#               ./autojohn.sh --info
 #               ./autojohn.sh --sessions
 #               ./autojohn.sh --show <SESSION_NAME>
 #               ./autojohn.sh --rules
 #               ./autojohn.sh --polish
+#               ./autojohn.sh --clean
 #
 #
 # --TODO--
 # - improve and optimize code
-# - better checks for command line parameters
 # - implement simple bruteforce:
 #     john test.txt --format=Raw-MD5 -1=?l -mask=?1?1?1?1?1?1?1 --fork=4
 #     ./autojohn.sh --bruteforce --min <MIN_LENGTH> --max 8 --mask <DLUA> <HASHES_FILE> <FORMAT> <SESSION_NAME>
@@ -31,14 +31,30 @@
 
 # VARIABLES --------------------------------------------------------------------
 
-DICT_DIR=~/DICTIONARIES     # each wordlist in this directory *MUST* be a ".txt" file
-POTS_DIR=~/.autojohn            # here you will find the cracked passwords for each session
+DICT_DIR=~/DICTIONARIES     # each dictionary/wordlist in this directory *MUST* be a ".txt" file
+POTS_DIR=~/.autojohn        # here you will find the cracked passwords from each session
+
+
+# OTHER VARIABLES (don't touch them!) ------------------------------------------
+
+OS=$(uname -s)
+
+if [[ $OS == "Linux" ]] ; then
+    DU_A_PARAM='--apparent-size'
+    CORES=$(grep -c ^processor /proc/cpuinfo)
+elif [[ $OS == "FreeBSD" ]] ; then
+    DU_A_PARAM='-A'
+    CORES=$(sysctl -n hw.ncpu)
+else
+    DU_A_PARAM=''
+    CORES=1
+fi
 
 
 # FUNCTIONS --------------------------------------------------------------------
 
 command_exists() {
-    command -v "$1" >/dev/null 2>&1 || { echo "Command not found: $1" 1>&2 ; exit 1 ; }
+    command -v "$1" > /dev/null 2>&1 || { echo "Command not found: $1" 1>&2 ; exit 1 ; }
 }
 
 logo() {
@@ -51,191 +67,185 @@ logo() {
 usage() {
     echo "Usage:"
     echo
-    echo "  - Show this help:"
     echo "    ./autojohn.sh [--help|-h]"
+    echo "    show this help"
     echo
-    echo "  - Show information about dictionaries:"
     echo "    ./autojohn.sh --info"
+    echo "    show some information about dictionaries"
     echo
-    echo "  - List detected hash formats for file <HASHES_FILE>:"
     echo "    ./autojohn.sh <HASHES_FILE>"
+    echo "    list detected hash formats for file <HASHES_FILE>"
     echo
-    echo "  - Start cracking hashes with dictionary attack:"
     echo "    ./autojohn.sh <HASHES_FILE> <FORMAT> <SESSION_NAME> [<RULE>]"
+    echo "    start cracking hashes with dictionary attack"
     echo "    (warning: with rules like \"EXTRA\" or \"ALL\" it may take *ages*)"
     echo
-    echo "  - Show sessions (both [F]inished and [R]unning):"
     echo "    ./autojohn.sh --sessions"
+    echo "    show sessions (both [F]inished and [R]unning)"
     echo
-    echo "  - Show currently found passwords in a session:"
     echo "    ./autojohn.sh --show <SESSION_NAME>"
+    echo "    show currently found passwords in session <SESSION_NAME>"
     echo
-    echo "  - List available (optional) rules:"
     echo "    ./autojohn.sh --rules"
+    echo "    list available (optional) JohnTheRipper rules"
     echo
-    echo "  - Clean all the dictionaries by removing non-printable characters"
-    echo "    and DOS newlines (CR-LF) and finally by (unique-)sorting them:"
     echo "    ./autojohn.sh --polish"
-    echo "    (warning: depending on the size of the dictionaries, it may take a"
-    echo "     very long time and require a lot of temporary disk space)"
+    echo "    clean all the dictionaries by removing non-printable characters"
+    echo "    and DOS newlines (CR-LF) and finally by (unique-)sorting them"
+    echo "    (warning: depending on the size of the dictionaries, it may take"
+    echo "    a very long time and require a lot of temporary disk space)"
     echo
+    echo "    ./autojohn.sh --clean"
+    echo "    delete all files in pots directory (except CSV with stats)"
+    echo
+
+    exit 0
 }
 
+info() {
+    DICT_NUM=$(ls -1 $DICT_DIR/*txt | wc -l | awk '{ print $1 }')
+    DICT_SIZ=$(du -ch $DU_A_PARAM $DICT_DIR/*txt | tail -1 | awk '{ print $1 }')
 
-# CHECKS -----------------------------------------------------------------------
+    echo "[+] Dictionaries directory:   $DICT_DIR"
+    echo "[+] Number of dictionaries:   $DICT_NUM"
+    echo "[+] Total dictionaries size:  $DICT_SIZ"
+    echo "[+] Pots directory:           $POTS_DIR"
+    echo
 
-declare -a CMDS=(
-"awk"
-"basename"
-"dos2unix"
-"john"
-"shasum"
-"tr"
-);
+    exit 0
+}
 
-for CMD in ${CMDS[@]} ; do
-    command_exists $CMD
-done
+sessions() {
+    N=$(ls -1 $POTS_DIR/*.pot 2> /dev/null | wc -l | awk '{ print $1 }')
 
-if [[ ! -d "$DICT_DIR" ]] ; then
-    echo "Error! Dictionaries directory not found: $DICT_DIR"
+    if [[ "$N" -eq 0 ]] ; then
+        echo "No sessions found (pots directory seems empty)."
+    else
+        for SESSION in $(ls -1 $POTS_DIR/*.pot | sed -r 's/.*\/(.*).pot.*/\1/') ; do
+            if [[ -f "$POTS_DIR/$SESSION.progress" ]] ; then
+                ps auxwww | grep john | grep -- "--session=$SESSION" > /dev/null
 
-    exit 1
-else
-    DICT_NUM=$(ls -1q $DICT_DIR/*.txt 2> /dev/null | wc -l)
+                if [[ $? -ne 0 ]] ; then
+                    echo "[R] $SESSION (dead?)"
+                else
+                    echo "[R] $SESSION"
+                fi
+            else
+                echo "[F] $SESSION"
+            fi
+        done
+    fi
 
-    if [[ "$DICT_NUM" -eq 0 ]] ; then
-        echo "Error! No *.txt dictionaries found!"
+    echo
+
+    exit 0
+}
+
+rules () {
+    john --list=rules | sort -h
+
+    echo
+
+    exit 0
+}
+
+polish() {
+    export LC_CTYPE=C
+    export LC_ALL=C
+
+    MEMORY_USAGE="50%" # maximum memory usage for "sort" command (think before changing it!)
+    CSV=$POTS_DIR/polished_dicts.csv
+
+    if [[ ! -w $CSV ]] ; then
+        echo "FILENAME;START_TIME;END_TIME;OLD_SIZE;NEW_SIZE" > $CSV
+    fi
+
+    for DICT in $(ls -1Sr $DICT_DIR/*.txt) ; do
+        BNDICT=$(basename $DICT)
+
+        echo "[>] $BNDICT"
+
+        STIME=$(date)
+        OLDSIZE=$(du -h $DU_A_PARAM $DICT | awk '{ print $1 }')
+
+        echo "    Started at:    $STIME"
+        echo "    Current size:  $OLDSIZE"
+
+        NEWDICT="${DICT}.NEW"
+
+        tr -dc '[:print:]\n\r' < $DICT > $NEWDICT
+        sleep 1
+        dos2unix $NEWDICT > /dev/null 2>&1
+        sleep 1
+        sort -S $MEMORY_USAGE -u $NEWDICT > $DICT 2>&1
+        sleep 1
+        rm $NEWDICT
+
+        NEWSIZE=$(du -h $DU_A_PARAM $DICT | awk '{ print $1 }')
+        ETIME=$(date)
+
+        echo "    New size:      $NEWSIZE"
+        echo "    Finished at:   $ETIME"
+        echo "$BNDICT;$STIME;$ETIME;$OLDSIZE;$NEWSIZE" >> $CSV
+        echo
+    done
+
+    echo "Results can also be found in the following CSV file:"
+    echo "$CSV"
+    echo
+
+    exit 0
+}
+
+clean() {
+    find $POTS_DIR -type f -not -name 'polished_dicts.csv' -delete
+
+    if [[ $? -eq 0 ]] ; then
+        echo "Pots directory was cleaned up."
+        echo
+
+        exit 0
+    else
+        echo "Error! Cannot clean pots directory: $POTS_DIR"
+        echo
 
         exit 1
     fi
-fi
+}
 
-if [[ ! -d "$POTS_DIR" ]] ; then
-    #echo "Error! Pots directory not found: $POTS_DIR"
-    #exit 1
-    mkdir -p "$POTS_DIR"
-fi
+show() {
+    SESSION=$1
 
+    PRG_FILE=$POTS_DIR/$SESSION.progress
+    CSV_FILE=$POTS_DIR/$SESSION.csv
 
-# MAIN -------------------------------------------------------------------------
+    if [[ -f "$PRG_FILE" ]] && [[ -s "$PRG_FILE" ]] ; then
+        echo "Found passwords in session \"$SESSION\"":
+        echo "---------------"
 
-if [[ "$#" -eq 0 || ! "$#" -le 4 ]] ; then
-    usage
+        # not so elegant but it works... need something better btw!
+        cat $PRG_FILE | grep -e '(.*)' | grep -v 'DONE (' | grep -v '^Loaded' | grep -v '^Node numbers'
 
-    exit 1
-else
-    PARAM=$1
+        echo
+    elif [[ -f "$CSV_FILE" ]] && [[ -s "$CSV_FILE" ]] ; then
+        echo "Found passwords in session \"$SESSION\"":
+        echo "---------------"
 
-    if [[ $OS == "Linux" ]] ; then
-        DU_A_PARAM='--apparent-size'
-    elif [[ $OS == "FreeBSD" ]] ; then
-        DU_A_PARAM='-A'
+        cat $CSV_FILE
+
+        echo
     else
-        DU_A_PARAM=''
+        echo "No passwords found (at the moment!) for session \"$SESSION\"."
+        echo
     fi
+}
 
-    if [[ "$#" -eq 1 ]] ; then
-        logo
+crack() {
+    PARAMS_COUNT=$(echo "$PARAMS" | wc -w)
+    PARAMS_ARRAY=($PARAMS)
 
-        if [[ "$PARAM" == "--help" || "$PARAM" == "-h" ]] ; then
-            usage
-
-            exit 0
-        elif [[ "$PARAM" == "--info" ]] ; then
-            DICT_NUM=$(ls -1 $DICT_DIR/*txt | wc -l | awk '{ print $1 }')
-            DICT_SIZ=$(du -ch $DU_A_PARAM $DICT_DIR/*txt | tail -1 | awk '{ print $1 }')
-
-            echo "[+] Dictionaries directory:   $DICT_DIR"
-            echo "[+] Number of dictionaries:   $DICT_NUM"
-            echo "[+] Total dictionaries size:  $DICT_SIZ"
-            echo
-
-            exit 0
-        elif [[ "$PARAM" == "--sessions" ]] ; then
-            N=$(ls -1 $POTS_DIR/*.pot 2> /dev/null | wc -l | awk '{ print $1 }')
-
-            if [[ "$N" -eq 0 ]] ; then
-                echo "No sessions found (pots directory seems empty)."
-            else
-                for SESSION in $(ls -1 $POTS_DIR/*.pot | sed -r 's/.*\/(.*).pot.*/\1/') ; do
-                    if [[ -f "$POTS_DIR/$SESSION.progress" ]] ; then
-                        ps auxwww | grep john | grep -- "--session=$SESSION" > /dev/null
-
-                        if [[ $? -ne 0 ]] ; then
-                            echo "[R] $SESSION (dead?)"
-                        else
-                            echo "[R] $SESSION"
-                        fi
-                    else
-                        echo "[F] $SESSION"
-                    fi
-                done
-            fi
-
-            echo
-
-            exit 0
-        elif [[ "$PARAM" == "--rules" ]] ; then
-            john --list=rules | sort -h
-
-            echo
-
-            exit 0
-        elif [[ "$PARAM" == "--polish" ]] ; then
-            export LC_CTYPE=C
-            export LC_ALL=C
-
-            MEMORY_USAGE="50%" # maximum memory usage for sort command
-            CSV=$POTS_DIR/polished_dicts.csv
-
-            echo "FILENAME;START_TIME;END_TIME;OLD_SIZE;NEW_SIZE" > $CSV
-
-            for DICT in $(ls -1Sr $DICT_DIR/*.txt) ; do
-                BNDICT=$(basename $DICT)
-
-                echo "[>] $BNDICT"
-
-                STIME=$(date)
-                OLDSIZE=$(du -sh $DU_A_PARAM $DICT | awk '{ print $1 }')
-
-                echo "    Started at:    $STIME"
-                echo "    Current size:  $OLDSIZE"
-
-                NEWDICT="${DICT}.NEW"
-
-                tr -dc '[:print:]\n\r' < $DICT > $NEWDICT
-                sleep 1
-                dos2unix $NEWDICT > /dev/null 2>&1
-                sleep 1
-                sort -S $MEMORY_USAGE -u $NEWDICT > $DICT 2>&1
-                sleep 1
-                rm $NEWDICT
-
-                NEWSIZE=$(du -sh $DU_A_PARAM $DICT | awk '{ print $1 }')
-                ETIME=$(date)
-
-                echo "    New size:      $NEWSIZE"
-                echo "    Finished at:   $ETIME"
-                echo "$BNDICT;$STIME;$ETIME;$OLDSIZE;$NEWSIZE" >> $CSV
-                echo
-            done
-
-            echo "Results can also be found in the following CSV file:"
-            echo "$CSV"
-            echo
-
-            exit 0
-        fi
-
-        if [[ $PARAM == --* ]] ; then
-            echo "Wrong parameter: $PARAM"
-            echo
-
-            exit 1
-        fi
-
-        FILE="$PARAM"
+    if [[ "$PARAMS_COUNT" -eq 1 ]] ; then
+        FILE="${PARAMS_ARRAY[0]}"
 
         if [[ ! -f "$FILE" ]] ; then
             echo "Error! Hashes file not found: $FILE"
@@ -270,54 +280,14 @@ else
         fi
 
         exit 0
-    elif [[ "$#" -eq 2 ]] ; then
-        logo
-
-        if [[ "$1" == "--show" ]] ; then
-            SESSION=$2
-
-            if [[ $SESSION == --* ]] ; then
-                echo "Wrong value for <SESSION_NAME>: $SESSION"
-                echo
-
-                exit 1
-            fi
-
-            PRG_FILE=$POTS_DIR/$SESSION.progress
-            CSV_FILE=$POTS_DIR/$SESSION.csv
-
-            if [[ -f "$PRG_FILE" ]] && [[ -s "$PRG_FILE" ]] ; then
-                echo "Found passwords in session \"$SESSION\"":
-                echo "---------------"
-
-                # not so elegant but it works... need something better btw!
-                cat $PRG_FILE | grep -e '(.*)' | grep -v 'DONE (' | grep -v '^Loaded' | grep -v '^Node numbers'
-
-                echo
-            elif [[ -f "$CSV_FILE" ]] && [[ -s "$CSV_FILE" ]] ; then
-                echo "Found passwords in session \"$SESSION\"":
-                echo "---------------"
-
-                cat $CSV_FILE
-
-                echo
-            else
-                echo "No passwords found (at the moment!) for session \"$SESSION\"."
-                echo
-            fi
-        fi
-
-        exit 0
-    elif [[ "$#" -eq 3 || "$#" -eq 4 ]] ; then
-        logo
-
-        FILE=$1
-        FORMAT=$2
-        SESSION=$3
+    elif [[ "$PARAMS_COUNT" -eq 3 || "$PARAMS_COUNT" -eq 4 ]] ; then
+        FILE="${PARAMS_ARRAY[0]}"
+        FORMAT="${PARAMS_ARRAY[1]}"
+        SESSION="${PARAMS_ARRAY[2]}"
         RULE=""
 
-        if [[ "$#" -eq 4 ]] ; then
-            RULE=$4
+        if [[ "$PARAMS_COUNT" -eq 4 ]] ; then
+            RULE="${PARAMS_ARRAY[3]}"
 
             if [[ $(john --list=rules | grep -c -i -w $RULE) -eq 0 ]] ; then
                 echo "Error! Rule does not exist: $RULE"
@@ -359,16 +329,6 @@ else
             echo
 
             exit 0
-        fi
-
-        OS=$(uname -s)
-
-        if [[ $OS == "Linux" ]] ; then
-            CORES=$(grep -c ^processor /proc/cpuinfo)
-        elif [[ $OS == "FreeBSD" ]] ; then
-            CORES=$(sysctl -n hw.ncpu)
-        else
-            CORES=1
         fi
 
         N=$(cat $FILE | wc -l | tr -d ' ')
@@ -439,4 +399,109 @@ else
 
         exit 0
     fi
+}
+
+
+# CHECKS -----------------------------------------------------------------------
+
+declare -a CMDS=(
+"awk"
+"basename"
+"dos2unix"
+"john"
+"shasum"
+"tr"
+);
+
+for CMD in ${CMDS[@]} ; do
+    command_exists $CMD
+done
+
+if [[ ! -d "$DICT_DIR" ]] ; then
+    echo "Error! Dictionaries directory not found: $DICT_DIR"
+
+    exit 1
+else
+    DICT_NUM=$(ls -1q $DICT_DIR/*.txt 2> /dev/null | wc -l)
+
+    if [[ "$DICT_NUM" -eq 0 ]] ; then
+        echo "Error! No *.txt dictionaries found."
+
+        exit 1
+    fi
 fi
+
+if [[ ! -d "$POTS_DIR" ]] ; then
+    mkdir -p "$POTS_DIR" 2> /dev/null
+
+    if [[ "$?" -ne 0 ]] ; then
+        echo "Error! Cannot create pots directory: $POTS_DIR"
+
+        exit 1
+    fi
+fi
+
+
+# MAIN -------------------------------------------------------------------------
+
+logo
+
+if [[ "$#" -eq 0 ]] ; then
+    usage
+fi
+
+PARAMS=""
+
+while (( "$#" )) ; do
+  case "$1" in
+    -h|--help)
+        usage
+        shift
+        ;;
+    --clean)
+        clean
+        shift
+        ;;
+    --info)
+        info
+        shift
+        ;;
+    --polish)
+        polish
+        shift
+        ;;
+    --rules)
+        rules
+        shift
+        ;;
+    --sessions)
+        sessions
+        shift
+        ;;
+    --show)
+        if [ -n "$2" ] && [ ${2:0:1} != "-" ] ; then
+            show $2
+            shift 2
+        else
+            echo "Error! Argument for $1 is missing." >&2
+            echo
+
+            exit 1
+        fi
+        ;;
+    -*|--*=) # unsupported flags
+        echo "Error! Unsupported flag: $1" >&2
+        echo
+
+        exit 1
+        ;;
+    *) # preserve positional arguments
+        PARAMS="$PARAMS $1"
+        shift
+        ;;
+    esac
+done
+
+eval set -- "$PARAMS"
+
+crack $PARAMS
